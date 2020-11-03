d3b3c8d34707e17a9831b87982c185319371d7f2
Erik Claesson <ec222qs@student.lnu.se>
2020-11-03 10:17:04 +0100

    Change route

--- src/routes/AuthRoute.tsx
+++ src/routes/AuthRoute.tsx
@@ -1,26 +1,31 @@
-// FIXME: Remove this later
+// TODO: Remove this later
 // @ts-nocheck
 
 import React, { useContext } from "react"
-import { Redirect, Route } from "react-router-dom"
+import { Redirect } from "react-router-dom"
 
 import { AuthContext } from "../context"
 
-// Fix this component, it errors on login
+// FIXME: This currently has an issue where authContext is taking some time to load
 
 export const AuthRoute = (props: any) => {
-  const { component: Component, ...rest } = props
+  const { component: Component } = props
   const authContext = useContext(AuthContext)
 
   console.log("AUTHROUTE USERNAME", authContext.authState?.userInfo?.username)
 
   if (authContext.authState?.userInfo?.username) {
-    return (
-      <Route {...rest} component={Component}>
-        <Component {...props} />
-      </Route>
-    )
+    return <Component {...props} />
   } else {
-    return <Redirect to="/login" />
+    return (
+      <Redirect
+        to={{
+          pathname: "/login",
+          state: {
+            from: props.location
+          }
+        }}
+      />
+    )
   }
 }
